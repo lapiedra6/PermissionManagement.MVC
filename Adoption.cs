598ca9ea01f53b4006a33ca5e972f913024c7f7d
marosvie <109626371+marosvie@users.noreply.github.com>
2022-07-31 23:26:25 -0500

    Adopciones

---
+++ Adoption.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using PermissionManagement.MVC.Controllers;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PermissionManagement.MVC.Models
+{
+    public class Adoption
+    {
+        [ScaffoldColumn(false)]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public string Id { get; set; }
+        public Status? Status { get; set; }
+        public string Name { get; set; }
+        public virtual Pet Pet{ get; set; }
+        public string AdoptingUser { get; set; }
+        public virtual IdentityUser IdentityUser { get; set; }
+    }
+}
