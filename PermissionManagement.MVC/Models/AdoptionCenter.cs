32e67ce887464ec8c55e7148318ec6847d135c6a
agdominguez <109626090+agdominguez@users.noreply.github.com>
2022-07-31 23:40:12 -0500

    Clase de refugios

---
+++ PermissionManagement.MVC/Models/AdoptionCenter.cs
@@ -0,0 +1,16 @@
+﻿using PermissionManagement.MVC.Controllers;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PermissionManagement.MVC.Models
+{
+    public class AdoptionCenter
+    {
+        [ScaffoldColumn(false)]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public virtual ICollection<Pet> Pets { get; set; }
+    }
+}
