d5ca18802f5051209b5810212fdfb785cf695510
aalmache1978 <108440641+aalmache1978@users.noreply.github.com>
2022-07-31 23:28:04 -0500

    Migraciones de datos para los modelos

---
+++ PermissionManagement.MVC/Migrations/20220731070709_Pets.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PermissionManagement.MVC.Migrations
+{
+    public partial class Pets : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Pet",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "varchar(255)", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Status = table.Column<int>(type: "int", nullable: true),
+                    Name = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Weight = table.Column<decimal>(type: "decimal(65,30)", nullable: false),
+                    Size = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pet", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Pet");
+        }
+    }
+}
---
+++ PermissionManagement.MVC/Migrations/20220731071152_PetsUpdated.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PermissionManagement.MVC.Migrations
+{
+    public partial class PetsUpdated : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AdoptionCenterId",
+                table: "Pet",
+                type: "varchar(255)",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pet_AdoptionCenterId",
+                table: "Pet",
+                column: "AdoptionCenterId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Pet_AdoptionCenter_AdoptionCenterId",
+                table: "Pet",
+                column: "AdoptionCenterId",
+                principalTable: "AdoptionCenter",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Pet_AdoptionCenter_AdoptionCenterId",
+                table: "Pet");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Pet_AdoptionCenterId",
+                table: "Pet");
+
+            migrationBuilder.DropColumn(
+                name: "AdoptionCenterId",
+                table: "Pet");
+        }
+    }
+}
