da56b0b0bff531ca613ce3ca9e2c3c0541dd31c5
lapiedra6 <109625721+lapiedra6@users.noreply.github.com>
2022-07-31 22:59:07 -0500

    Creación del modelo Pet para las mascotas.

---
+++ PermissionManagement.MVC/Models/Pet.cs
@@ -0,0 +1,31 @@
+﻿using PermissionManagement.MVC.Controllers;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PermissionManagement.MVC.Models
+{
+    public class Pet
+    {
+        [ScaffoldColumn(false)]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public string Id { get; set; }
+        public Status? Status { get; set; }
+        public string Name { get; set; }
+        public decimal Weight { get; set; }
+        public Size? Size { get; set; }
+        public virtual AdoptionCenter AdoptionCenter { get; set; }
+        [NotMapped]
+        public List<AdoptionCenter> Centers { get; set; }
+    }
+    public enum Status
+    {
+        Pendiente,
+        Revision,
+        Adoptado
+    }
+    public enum Size
+    {
+        Pequeno,Mediano,Grande
+    }
+}
